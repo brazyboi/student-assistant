981040d7965f71fd8018bc0e3e94d27b868def6e
Lucas Zhong <zhonglucas@gmail.com>
2025-12-18 00:18:58 -0800

    Simplify MarkdownRenderer more

--- client/src/lib/Markdown.tsx
+++ client/src/lib/Markdown.tsx
@@ -6,7 +6,7 @@ import 'katex/dist/katex.min.css';
 
 const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
   const processedMarkdown = markdown
-    .replace(/\n{3,}/g, '\n\n'); 
+    .replace(/\n{3,}/g, '\n\n');
     
     return (
     <div className="
@@ -22,23 +22,10 @@ const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
       prose-headings:mt-4 prose-headings:mb-2
       prose-p:my-2
       prose-ul:my-2 prose-li:my-0
-      
-      [&_.math-inline]:mx-1
-      [&_.math-display]:my-2
     ">
       <ReactMarkdown
-        remarkPlugins={[[remarkMath, { singleDollarText: true }], remarkGfm]}
+        remarkPlugins={[[remarkMath, { singleDollarText: false }], remarkGfm]}
         rehypePlugins={[rehypeKatex]}
-        components={{
-          code({ className, children }) {
-            const language = className?.replace('language-', '') || '';
-            return (
-              <code className={className}>
-                {children}
-              </code>
-            );
-          },
-        }}
       >
         {processedMarkdown}
       </ReactMarkdown>
