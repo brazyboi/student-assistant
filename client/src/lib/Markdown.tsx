a3f05eeb8817c620aa1a40218d397948c981ed1a
Lucas Zhong <zhonglucas@gmail.com>
2025-12-18 00:17:43 -0800

    Update markdown renderer to be less aggressive

--- client/src/lib/Markdown.tsx
+++ client/src/lib/Markdown.tsx
@@ -6,13 +6,9 @@ import 'katex/dist/katex.min.css';
 
 const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
   const processedMarkdown = markdown
-    .replace(/\n+/g, '\n')
-    .replace(/([^\n])(#{1,6}\s)/g, '$1\n$2')
-    .replace(/(^|\n)\s*(\d+\.)\s+(?=\*\*)/g, '$1### $2 ')
-    .replace(/([^\n])\s+(-|\*|\d+\.)\s/g, '$1\n$2 ');  
+    .replace(/\n{3,}/g, '\n\n'); 
     
     return (
-
     <div className="
       prose prose-slate dark:prose-invert 
       max-w-none break-words
@@ -26,10 +22,23 @@ const MarkdownRenderer = ({ markdown }: { markdown: string }) => {
       prose-headings:mt-4 prose-headings:mb-2
       prose-p:my-2
       prose-ul:my-2 prose-li:my-0
+      
+      [&_.math-inline]:mx-1
+      [&_.math-display]:my-2
     ">
       <ReactMarkdown
-        remarkPlugins={[[remarkMath, { singleDollarText: false }], remarkGfm]}
+        remarkPlugins={[[remarkMath, { singleDollarText: true }], remarkGfm]}
         rehypePlugins={[rehypeKatex]}
+        components={{
+          code({ className, children }) {
+            const language = className?.replace('language-', '') || '';
+            return (
+              <code className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
       >
         {processedMarkdown}
       </ReactMarkdown>
